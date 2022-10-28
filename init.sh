d92329774dc5149eb38afd2db0e5c467bcee3339
Vladislav <sorokinvld@gmail.com>
2022-10-28 20:58:16 +0300

    cmt

---
+++ init.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/matrix-org/matrix-js-sdk.git
+cd matrix-js-sdk
+git checkout robertlong/group-call
+yarn
+yarn link
+cd ..
+yarn
+yarn link matrix-js-sdk
+cp .env.example .env
+# yarn dev
+yarn run build
